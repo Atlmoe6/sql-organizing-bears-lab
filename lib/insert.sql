290284bcf4ea72f344b03439ff1cacbcfc4d3bdf
Doe Mohamud <doe.mahamud1@gmail.com>
2021-06-20 22:49:50 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 30, "M", "Black", "Timid", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 20, "M", "Black", "Timid", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 10, "F", "Black", "Timid", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 20, "M", "Black", "Timid", 0);
+INSERT INTO bears VALUES (5, "Melissa", 10, "F", "Black", "Timid", 1);
+INSERT INTO bears VALUES (6, "Grinch", 40, "M", "Black", "Timid", 1);
+INSERT INTO bears VALUES (7, "Wendy", 50, "F", "Black", "Timid", 0);
+INSERT INTO bears VALUES (8, null, 60, "M", "Black", "Timid", 0);
